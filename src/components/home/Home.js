4c64030b3ceb726be160316ca7741bdc37f223f2
mohddarain2 <mohddarain2@gmail.com>
2023-02-07 18:24:49 +0530

    darain

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Countries from "./Countries";
+import Fake_footer from "./Fake_footer";
 
 function Home() {
   return (
     <>
        <Header /> 
        <Countries />
+       <Fake_footer/>
     </>
   );
 }
