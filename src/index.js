16584d4e9f366028e7a4f822a257c1288e754bcb
Danish <alidanish936@gmail.com>
2023-02-09 17:24:00 +0530

    danish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Mobile from './components/home/Mobile';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App />
+    <Mobile />
   
 );
 
